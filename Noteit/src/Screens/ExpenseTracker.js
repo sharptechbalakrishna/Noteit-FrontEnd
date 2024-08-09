a0aad4b4306ff740e60f16041905e28314854669
unknown <sharptechbalakrishna@gmail.com>
2024-08-09 18:55:31 +0530

    Gayathri

--- Noteit/src/Screens/ExpenseTracker.js
+++ Noteit/src/Screens/ExpenseTracker.js
@@ -1,19 +1,22 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { View, Text, TextInput, Button, FlatList, StyleSheet } from 'react-native';
+import { View, Text, TextInput, Button, SectionList, StyleSheet, TouchableOpacity } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import axios from 'axios';
+import UserService from '../UserService/UserService';
 import { AuthContext } from '../Context/AuthContext';
+import { format } from 'date-fns';
 
-const BASE_URL = 'http://192.168.3.53:8080';
 
 const ExpenseTracker = () => {
   const { userInfo } = useContext(AuthContext);
   const [description, setDescription] = useState('');
   const [amount, setAmount] = useState('');
   const [transactions, setTransactions] = useState([]);
+  const [filteredTransactions, setFilteredTransactions] = useState([]);
   const [savings, setSavings] = useState(0);
   const [spentAmount, setSpentAmount] = useState(0);
   const [income, setIncome] = useState(0);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [isSearching, setIsSearching] = useState(false);
 
   useEffect(() => {
     if (userInfo && userInfo.id) {
@@ -21,34 +24,46 @@ const ExpenseTracker = () => {
     }
   }, [userInfo]);
 
+  useEffect(() => {
+    handleSearch();
+  }, [searchQuery, transactions]);
+
   const fetchExpenseTrackerData = async (customerId) => {
     try {
-      const response = await axios.get(`${BASE_URL}/get-expense-tracker`, {
-        params: { customerId },
-      });
-      console.log('Fetched Expense Tracker Data:', response.data);
-
-      const sortedTransactions = response.data.transactions.sort((a, b) => new Date(b.createdTs) - new Date(a.createdTs));
-
+      const data = await UserService.fetchExpenseTrackerData(customerId);
+      const sortedTransactions = (data.transactions || []).sort(
+        (a, b) => new Date(b.updatedTs) - new Date(a.updatedTs)
+      );
       setTransactions(sortedTransactions);
-      setSavings(response.data.savings);
-      setSpentAmount(response.data.spentAmount);
-      setIncome(response.data.income);
+      setFilteredTransactions(sortedTransactions); // Initialize with all transactions
+      setSavings(data.savings || 0);
+      setSpentAmount(data.spentAmount || 0);
+      setIncome(data.income || 0);
     } catch (error) {
       console.error('Error fetching expense tracker:', error);
     }
   };
 
+  const handleSearch = () => {
+    if (searchQuery.trim() === '') {
+      setFilteredTransactions(transactions);
+    } else {
+      const filtered = transactions.filter(transaction =>
+        transaction.description.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+      setFilteredTransactions(filtered);
+    }
+  };
+
   const handleAddIncome = async () => {
     if (description && amount) {
-      console.log('Add Income - Description:', description, 'Amount:', amount);
       const expenseData = {
         customerId: userInfo.id,
         description,
         income: parseFloat(amount),
       };
       try {
-        await axios.post(`${BASE_URL}/update-expense`, expenseData);
+        await UserService.updateExpense(expenseData);
         await fetchExpenseTrackerData(userInfo.id); // Refresh data after adding income
         setDescription('');
         setAmount('');
@@ -60,14 +75,13 @@ const ExpenseTracker = () => {
 
   const handleAddExpense = async () => {
     if (description && amount) {
-      console.log('Add Expense - Description:', description, 'Amount:', amount);
       const expenseData = {
         customerId: userInfo.id,
         description,
         spentAmount: parseFloat(amount),
       };
       try {
-        await axios.post(`${BASE_URL}/update-expense`, expenseData);
+        await UserService.updateExpense(expenseData);
         await fetchExpenseTrackerData(userInfo.id); // Refresh data after adding expense
         setDescription('');
         setAmount('');
@@ -77,26 +91,38 @@ const ExpenseTracker = () => {
     }
   };
 
+  const groupedTransactions = filteredTransactions.reduce((groups, transaction) => {
+    const date = format(new Date(transaction.updatedTs), 'dd MMM yyyy');
+    if (!groups[date]) {
+      groups[date] = [];
+    }
+    groups[date].push(transaction);
+    return groups;
+  }, {});
+
+  const sections = Object.keys(groupedTransactions).map(date => ({
+    title: date,
+    data: groupedTransactions[date],
+  }));
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Expense Tracker</Text>
       <View style={styles.summaryContainer}>
-      <View style={styles.summaryCard}>
+        <View style={styles.summaryCard}>
           <Text style={styles.summaryTitle}>Balance</Text>
-          <Text style={styles.balanceText}>{`₹${savings.toFixed(2)}`}</Text>
+          <Text style={styles.balanceText}>{savings ? ` ₹ ${savings.toFixed(2)}` : '0.00'}</Text>
         </View>
         <View style={styles.summaryCard}>
           <Text style={styles.summaryTitle}>Income</Text>
-          <Text style={styles.incomeText}>{`₹${income.toFixed(2)}`}</Text>
+          <Text style={styles.incomeText}>{income ? `₹${income.toFixed(2)}` : '0.00'}</Text>
         </View>
         <View style={styles.summaryCard}>
           <Text style={styles.summaryTitle}>Expenses</Text>
-          <Text style={styles.expenseText}>{`₹${spentAmount.toFixed(2)}`}</Text>
+          <Text style={styles.expenseText}>{spentAmount ? `₹${spentAmount.toFixed(2)}` : '0.00'}</Text>
         </View>
-       
       </View>
 
-      {/* First Row: Description and Add Income Button */}
       <View style={styles.formRow}>
         <TextInput
           style={[styles.input, styles.inputDescription]}
@@ -107,7 +133,6 @@ const ExpenseTracker = () => {
         <Button title="Income  " onPress={handleAddIncome} color="#4CAF50" />
       </View>
 
-      {/* Second Row: Amount and Add Expense Button */}
       <View style={styles.formRow}>
         <TextInput
           style={[styles.input, styles.inputAmount]}
@@ -120,19 +145,42 @@ const ExpenseTracker = () => {
       </View>
 
       <View style={styles.iconsRow}>
-        <Ionicons name="search-outline" size={24} color="black" />
+        {isSearching ? (
+          <View style={styles.searchContainer}>
+            <TextInput
+              style={styles.searchInput}
+              placeholder="Search transactions..."
+              value={searchQuery}
+              onChangeText={setSearchQuery}
+            />
+            <TouchableOpacity onPress={() => setIsSearching(false)}>
+              <Ionicons name="close-outline" size={24} color="black" />
+            </TouchableOpacity>
+          </View>
+        ) : (
+          <TouchableOpacity onPress={() => setIsSearching(true)}>
+            <Ionicons name="search-outline" size={24} color="black" />
+          </TouchableOpacity>
+        )}
         <Ionicons name="filter-outline" size={24} color="black" />
       </View>
 
-      <FlatList
-        data={transactions}
+      <SectionList
+        sections={sections}
         keyExtractor={(item) => item.id.toString()}
         renderItem={({ item }) => (
           <View style={styles.transactionItem}>
-            <Text style={styles.transactionDescription}>{item.description}</Text>
-            <Text style={item.income ? styles.incomeAmount : styles.expenseAmount}>
-              {item.income ? `₹${item.income.toFixed(2)}` : `₹${item.spentAmount.toFixed(2)}`}
-            </Text>
+            <View style={styles.transactionDetails}>
+              <Text style={styles.transactionDescription}>{item.description}</Text>
+              <Text style={item.income ? styles.incomeAmount : styles.expenseAmount}>
+                {item.income ? `₹${item.income.toFixed(2)}` : `₹${item.spentAmount.toFixed(2)}`}
+              </Text>
+            </View>
+          </View>
+        )}
+        renderSectionHeader={({ section: { title } }) => (
+          <View style={styles.sectionHeader}>
+            <Text style={styles.sectionTitle}>{title}</Text>
           </View>
         )}
       />
@@ -176,7 +224,7 @@ const styles = StyleSheet.create({
   },
   balanceText: {
     fontSize: 16,
-    color: '#333',
+    color: '#d98947',
     paddingBottom: 10,
   },
   incomeText: {
@@ -206,7 +254,7 @@ const styles = StyleSheet.create({
   },
   input: {
     height: 40,
-    borderColor: 'black',
+    borderColor: '#948e8d',
     borderWidth: 1,
     borderRadius: 5,
     paddingHorizontal: 10,
@@ -219,9 +267,10 @@ const styles = StyleSheet.create({
     marginHorizontal: 10,
     marginBottom: 20,
     backgroundColor: "white",
-    borderRadius: 5,
-    borderWidth: 1,
-    padding: 10,
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    alignItems: "center",
   },
   transactionItem: {
     flexDirection: 'row',
@@ -233,26 +282,69 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     borderRadius: 8,
     marginHorizontal: 10,
-    marginBottom: 10,
-    elevation: 2,
+    marginBottom: 5,
   },
+  transactionDetails: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flex: 1,
+  },
+  
   transactionDescription: {
     fontSize: 16,
     color: '#333',
-    flex: 1,
+  flex: 1,
+
   },
   incomeAmount: {
     fontSize: 16,
     color: '#4CAF50',
-    fontWeight: 'bold',
+     fontWeight: 'bold',
     textAlign: 'right',
   },
   expenseAmount: {
     fontSize: 16,
-    color: '#F44336',
-    fontWeight: 'bold',
+    color: '#F44336', 
+       fontWeight: 'bold',
+
     textAlign: 'right',
   },
+  transactionDate: {
+    fontSize: 12,
+    color: '#aaa',
+
+  },
+  sectionHeader: {
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    justifyContent:'center',
+    alignItems:'center',
+  },
+  sectionTitle: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#333',
+    backgroundColor:'#eae5e5',
+    padding:5,
+    borderRadius:5,
+  },
+  searchContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+   
+    paddingHorizontal: 0,
+    borderRadius: 10,
+  },
+  searchInput: {
+    flex: 1,
+    height: 40,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    paddingHorizontal: 10,
+  },
+ 
 });
 
 export default ExpenseTracker;
--- Noteit/src/UserService/UserService.js
+++ Noteit/src/UserService/UserService.js
@@ -56,41 +56,31 @@ class UserService {
         }
     }
 
-    // Adding the new notes for the perticular customer id
-    static async addingNotes(customerId) {
+
+    // Fetching Expense Tracker Data
+    static async fetchExpenseTrackerData(customerId) {
         try {
-            console.log(" Adding Notes :", customerId);
-            const response = await axios.get(`http://192.168.3.53:8080/${customerId}/selfnotes`);
-            console.log("Status");
-            console.log(response.data);
-            console.log("Status", response.status);
-            return response;
+            const response = await axios.get(`${UserService.BASE_URL}/get-expense-tracker`, {
+                params: { customerId },
+            });
+            return response.data;
         } catch (err) {
             throw err;
         }
     }
 
-    // static async borrowerData(userInfo, borrowerData) {
-    //     try {
-    //         const response = await fetch(`${UserService.BASE_URL}/${userInfo.id}/borrowerData`, {
-    //             method: 'POST',
-    //             headers: {
-    //                 'Content-Type': 'application/json',
-    //             },
-    //             body: JSON.stringify(borrowerData),
-    //         });
+    // Update Expense (either add income or expense)
+    static async updateExpense(expenseData) {
+        try {
+            const response = await axios.post(`${UserService.BASE_URL}/update-expense`, expenseData);
+            return response.data;
+        } catch (err) {
+            throw err;
+        }
+    }
 
-    //         if (!response.ok) {
-    //             throw new Error('Failed to add borrower');
-    //         }
 
-    //         const data = await response.json();
-    //         return data;
-    //     } catch (error) {
-    //         console.error('Error adding borrower:', error);
-    //         throw error;
-    //     }
-    // }
+
 }
 
 export default UserService;
